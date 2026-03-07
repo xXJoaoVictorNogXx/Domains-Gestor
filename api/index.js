60035b53966d893c7430558d745e615fd0fb60e3
xXJoaoVictorNogXx <joaovictorfcantanheden@gmail.com>
2026-03-07 00:29:16 -0300

    funciona

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const jsonServer = require('json-server');
-const path = require('path'); // Adicione isso
+const path = require('path');
 const server = jsonServer.create();
 
-const router = jsonServer.router(path.join(__dirname, '../db/db.json')); 
+// Rota de teste para forçar log
+server.get('/api/test', (req, res) => {
+  console.log("LOG DE TESTE: A função foi chamada!");
+  res.json({ message: "API funcionando na Vercel!" });
+});
+
+const dbPath = path.join(process.cwd(), 'db', 'db.json');
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1'
-}));
+server.use(jsonServer.rewriter({ '/api/*': '/$1' }));
 server.use(router);
 
 module.exports = server;
